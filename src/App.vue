e0d8ed6a7b02364c134bbc6297848bf426f3be1f
Sameer <sameergaikwad222@gmail.com>
2020-06-22 21:51:20 +0530

    First Upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="b-container-fluid w-25 m-auto">
+      <b-card>
+        <b-card-text>
+          <transition name="slide-fade">
+            <quoteCard :Quote="this.currentQuote" v-if="show" />
+          </transition>
+        </b-card-text>
+        <b-button
+          @click="getlatestQuote"
+          variant="primary"
+          style="font-family: Avenir, Helvetica, Arial, sans-serif;"
+        >Next Quote</b-button>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import quoteCard from "./components/quoteCard.vue";
+import quotes from "@/data/quotes.js";
 export default {
-  name: 'App',
+  name: "App",
+  data: function() {
+    return {
+      random: 0,
+      currentQuote: quotes[this.random],
+      show: true
+    };
+  },
   components: {
-    HelloWorld
+    quoteCard
+  },
+  methods: {
+    getlatestQuote() {
+      this.show = false;
+      var num = Math.floor(Math.random() * quotes.length);
+      this.currentQuote = quotes[num];
+    }
+  },
+  quotes,
+  beforeCreate: function() {
+    this.random = Math.floor(Math.random() * quotes.length);
+  },
+  updated: function() {
+    this.show = true;
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Piedra", cursive;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
---
+++ src/components/quoteCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{Quote.text}}</h1>
+    <p style="font-family: Avenir, Helvetica, Arial, sans-serif;"><strong>-- {{Quote.author}}</strong></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    Quote: Object
+  },
+};
+</script>
+
+<style scoped>
+  p{
+    margin-left: 50%;
+  }
+</style>>
+
